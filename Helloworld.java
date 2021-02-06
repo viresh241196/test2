2bd7f873e8361fae2522c224d3b27625e3a0cb06
viresh rawool <vireshrawool007@gmail.com>
2021-02-06 10:07:16 +0530

    added all files

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+package hellojava;
+import java.util.*;
+public class Helloworld {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		System.out.print("hello world");
+		
+	}
+
+}
+
---
+++ New.java
@@ -0,0 +1,8 @@
+package hellojava;
+
+public class New {
+	public static void main(String[] args ) {
+		System.out.println("hello world");
+	}
+	
+	}
