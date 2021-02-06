f3f4b1c9c5b318321f7a2c522c3b30d65c8bedb9
viresh rawool <vireshrawool007@gmail.com>
2021-02-06 16:40:18 +0530

    edited the helloworld

--- Helloworld.java
+++ Helloworld.java
@@ -1,9 +1,6 @@
 package hellojava;
-import java.util.*;
 public class Helloworld {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int num=sc.nextInt();
 		System.out.print("hello world");
 		System.out.print("some thing new");
 		
