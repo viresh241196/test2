3375a883b20ff7e56616442ffcb3eb121322e301
viresh rawool <vireshrawool007@gmail.com>
2021-02-06 13:02:06 +0530

    made changes at local to create a conflit

--- New.java
+++ New.java
@@ -3,7 +3,7 @@ package hellojava;
 public class New {
 	public static void main(String[] args ) {
 		System.out.println("hello world");
-		System.out.println("new edit");
+		System.out.println("new edit at local");
 	}
 	
 	}
